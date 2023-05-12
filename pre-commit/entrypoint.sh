18344f24f4ff110f60cce590c770b048b5030b72
Jay Shen <jfshen@uw.edu>
2023-05-12 11:58:36 -0700

    CAB-5815 update deprecated actions to fix broken build

--- pre-commit/entrypoint.sh
+++ pre-commit/entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 
 cd "$GITHUB_WORKSPACE" || exit
 
+# the following "git config" is required to fix build failure
+# see https://github.com/actions/runner-images/issues/6775
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
 pre-commit run --all-files
